56849ab92ee5473943c8806c6053126160902f56
darthpedroo <pedrovillarino06@gmail.com>
2024-02-29 01:02:54 -0300

    Updated the code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,27 @@
 <script setup>
-import Counter from './Counter.vue';
-import DataFetcher from './DataFetcher.vue';
+import { getArticles } from '../utils/apiUtils';
+import {ref, onMounted} from 'vue'
+
+const response = ref(null)
+onMounted(() => getArticles(response))
+
 </script>
 
 <template>
   <div>
     <h1>Idiot World</h1>
-    <DataFetcher></DataFetcher>
-    <Counter></Counter>
-    <Counter></Counter>
-    <Counter></Counter>
+    
+    <div>
+        <div v-if="response != null && response.length > 0">
+            <li v-for="data in response" :key="response.id">
+                <h2> {{ data.title }}</h2>
+            </li>
+        </div>
+        <div v-else>
+            Data is fetching--
+        </div>
+    </div>
+
   </div>
 </template>
 
