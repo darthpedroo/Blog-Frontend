2db1f89c3b4a384f2c741b8f2a741bb6b15661de
darthpedroo <pedrovillarino06@gmail.com>
2024-03-12 20:53:07 -0300

    Added Callback after posting data. Closes #8

--- src/utils/postSingleParagraph.js
+++ src/utils/postSingleParagraph.js
@@ -1,9 +1,14 @@
 import ApiHandler from "@/services/ApiHandler";
 
-export function postSingleParagraph(body){
+export function postSingleParagraph(body, callback){
     ApiHandler.postSingleParagraphHandler(body)
     .then((response) => {
         console.log(response);
+
+        if(callback && typeof callback === 'function'){
+          callback()
+        }
+
       })
       .catch((error) => {
         console.error(error);
